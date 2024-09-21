2a5a165ff9934b98f2772e459410615153bc3ed5
vijay-15265 <vijayarajan.b@zohocorp.com>
2024-09-21 14:48:10 +0530

    Design corrections were done in Signup parent screen.

--- app/src/main/java/com/example/esevafood/SignUpComponenets.kt
+++ app/src/main/java/com/example/esevafood/SignUpComponenets.kt
@@ -1,10 +1,11 @@
 package com.example.esevafood
 
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,28 +24,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.esevafood.ui.theme.fragment.RegisterFragment
 
 
 @Preview
 @Composable
-fun SignUpLayout() {
+fun SignUpLayout(
+    onClick: () -> Unit = {}
+
+) {
     MaterialTheme {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color.White)
                 .padding(20.dp),
-            verticalArrangement = Arrangement.spacedBy(40.dp)
+            verticalArrangement = Arrangement.spacedBy(10.dp)
 
         ) {
 
             Column(
-                verticalArrangement = Arrangement.spacedBy(40.dp),
+                verticalArrangement = Arrangement.Bottom,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.5f),
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.hackathon_image_2),
@@ -53,8 +61,9 @@ fun SignUpLayout() {
                         .align(Alignment.CenterHorizontally)
                         .width(200.dp)
                         .height(220.dp)
-                        .padding(top = 50.dp)
+                        .padding(top = 50.dp),
                 )
+                Spacer(Modifier.height(20.dp))
 
                 Text(
                     text = "Register",
@@ -62,92 +71,129 @@ fun SignUpLayout() {
                     fontSize = 30.sp,
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
-            }
+                Spacer(Modifier.height(30.dp))
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally)
-                    .border(
-                        width = 1.dp,
-                        shape = RoundedCornerShape(8.dp),
-                        color = MaterialTheme.colorScheme.outline
-                    )
-                    .padding(top = 10.dp, bottom = 10.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.eseva_user_logo),
-                    contentDescription = "User"
-                )
-                Spacer(
-                    modifier = Modifier.width(10.dp)
-                )
                 Text(
-                    text = "Family/ Individual Donor",
+                    "Cutting food waste is a delicious way of saving money, helping to feed the world and protect the planet.",
+                    textAlign = TextAlign.Center
                 )
+                Spacer(Modifier.height(10.dp))
+
             }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(
-                        width = 1.dp,
-                        shape = RoundedCornerShape(8.dp),
-                        color = MaterialTheme.colorScheme.outline
-                    )
-                    .padding(top = 10.dp, bottom = 10.dp),
-                horizontalArrangement = Arrangement.Center
+            Column(
+                modifier = Modifier.weight(0.5f),
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                Image(
-                    painter = painterResource(R.drawable.eseva_user_org_logo),
-                    contentDescription = "User"
-                )
-                Spacer(
-                    modifier = Modifier.width(10.dp)
-                )
-                Text(
-                    text = "Organisation Donor",
-                )
-            }
-
-            Row(
-                horizontalArrangement = Arrangement.Absolute.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Spacer(
+                Row(
                     modifier = Modifier
-                        .height(1.dp)
-                        .background(
-                            color = Color.Black
+                        .fillMaxWidth()
+                        .border(
+                            width = 1.dp,
+                            shape = RoundedCornerShape(8.dp),
+                            color = MaterialTheme.colorScheme.outline
                         )
-                        .weight(1f)
-                )
-                Text(
-                    text = "or",
-                    fontSize = 15.sp,
-                    modifier = Modifier.padding(10.dp)
+                        .padding(top = 10.dp, bottom = 10.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.eseva_user_logo),
+                        contentDescription = "User"
+                    )
+                    Spacer(
+                        modifier = Modifier.width(10.dp)
+                    )
+                    Text(
+                        text = "Family/ Individual Donor",
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            width = 1.dp,
+                            shape = RoundedCornerShape(8.dp),
+                            color = MaterialTheme.colorScheme.outline
+                        )
+                        .padding(top = 10.dp, bottom = 10.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.eseva_user_org_logo),
+                        contentDescription = "User"
+                    )
+                    Spacer(
+                        modifier = Modifier.width(10.dp)
+                    )
+                    Text(
+                        text = "Organisation Donor",
+                    )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.Absolute.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Spacer(
+                        modifier = Modifier
+                            .height(1.dp)
+                            .background(
+                                color = Color.Black
+                            )
+                            .weight(1f)
+                    )
+                    Text(
+                        text = "or",
+                        fontSize = 15.sp,
+                        modifier = Modifier.padding(10.dp)
+                    )
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(1.dp)
+                            .background(
+                                color = Color.Black
+                            )
+                            .weight(1f)
+                    )
+                }
+                BottomButtonLarge(
+                    onClick = onClick
                 )
 
-                Spacer(
-                    modifier = Modifier
-                        .height(1.dp)
-                        .background(
-                            color = Color.Black
-                        )
-                        .weight(1f)
-                )
             }
 
-            Text(
-                text = "Sign in with password",
-                modifier = Modifier
-                    .background(color = Color(0xFF5F61F0), RoundedCornerShape(8.dp))
-                    .padding(25.dp),
-            )
 
         }
     }
+}
+
+@Composable
+fun BottomButtonLarge(
+    backgroundColour: Color = Color(0xFF5F61F0),
+    buttonName: String = "Sign in with password",
+    onClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = buttonName,
+            modifier = Modifier
+                .background(color = backgroundColour, RoundedCornerShape(8.dp))
+                .padding(top = 10.dp, bottom = 10.dp, start = 30.dp, end = 30.dp),
+            color = Color.White,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = androidx.compose.ui.text.style.TextAlign.Center
+        )
+    }
+
+
 }

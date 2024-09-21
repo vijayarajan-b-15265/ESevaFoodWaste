f05771cf052ee66324bb856413a6c21edad2109b
vijay-15265 <vijayarajan.b@zohocorp.com>
2024-09-21 16:04:16 +0530

    Implemented Register Screen.

--- app/src/main/java/com/example/esevafood/activities/MainActivity.kt
+++ app/src/main/java/com/example/esevafood/activities/MainActivity.kt
@@ -1,26 +1,26 @@
-package com.example.esevafood.ui.theme.fragment
+package com.example.esevafood.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import com.example.esevafood.composables.SignUpLayout
 
-class RegisterFragment : ComponentActivity(){
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MaterialTheme {
-                Greet()
+                val intent = Intent(this, RegisterActivity::class.java)
+                SignUpLayout(
+                    onClick = {
+                        startActivity(intent)
+                    }
+                )
             }
         }
     }
-}
-
-@Composable
-fun Greet(){
-    Text("Sign up here")
 }
--- app/src/main/java/com/example/esevafood/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/esevafood/activities/RegisterActivity.kt
@@ -1,25 +1,19 @@
-package com.example.esevafood
+package com.example.esevafood.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
-import com.example.esevafood.ui.theme.fragment.RegisterFragment
+import com.example.esevafood.composables.RegisterScreen
 
-class MainActivity : ComponentActivity() {
+class RegisterActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MaterialTheme {
-                val intent = Intent(this, RegisterFragment::class.java)
-                SignUpLayout(
-                    onClick = {
-                        startActivity(intent)
-                    }
-                )
+                RegisterScreen()
             }
         }
     }
